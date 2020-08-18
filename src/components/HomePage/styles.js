fc461ba6d15734d3c32e63fd2de9e72f70a062f4
V-Coyote <vinicius.almeida.r@gmail.com>
2020-08-18 06:09:33 -0300

    first commit

---
+++ src/components/HomePage/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const DirectoryMenu = styled.div``;
+
+export const MenuItem = styled.div``;
+
+export const Content = styled.div``;
