c08329b5212c4a72b6c71bbbf736c15620fc17b8
V-Coyote <vinicius.almeida.r@gmail.com>
2020-08-29 17:25:55 -0300

    FIX: shop-data.js with new data FEATURE: add new feature to redux when add item to cart

--- src/components/CollectionItem/index.jsx
+++ src/components/CollectionItem/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container, Image, Footer } from './styles';
+import { addItem } from '../../redux/cart/cart-actions';
 
-function CollectionItem({ id, name, price, imageUrl }) {
+function CollectionItem({ item }) {
+  const { id, name, price, imageUrl } = item;
+
+  const dispatch = useDispatch();
+
+  const dispatchItem = () => dispatch(addItem(item));
+
   return (
     <Container>
       <Image image={imageUrl}>
-        <button>ADD TO CART</button>
+        <button onClick={dispatchItem}>ADD TO CART</button>
       </Image>
       <Footer>
         <span>{name}</span>
--- src/components/PreviewCollection/index.jsx
+++ src/components/PreviewCollection/index.jsx
@@ -12,8 +12,8 @@ function PreviewCollection({ title, routeName, items }) {
         <Grid>
           {items
             .filter((item, index) => index < 4)
-            .map(({ id, ...otherProps }) => {
-              return <CollectionItem key={id} {...otherProps} />;
+            .map((item) => {
+              return <CollectionItem key={item.id} item={item} />;
             })}
         </Grid>
       </Container>
